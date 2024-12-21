b702de0ab93c466c5f87e75822a670adcfc02f8a
LabyrinthWarden <46149695+LabyrinthWarden@users.noreply.github.com>
2024-12-20 23:22:37 -0800

    more changes

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -3,12 +3,18 @@
 
 </script>
 
-<div class="my-4 w-full text-center">
-	<h1 class="text-4xl">Calendar</h1>
+<!-- Hero Section -->
+<div class="hero bg-base-200">
+	<div class="hero-content py-12 text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Calendar</h1>
+		</div>
+	</div>
 </div>
 <div class="flex w-full">
 	<div class="responsiveCal mx-auto">
 		<div class="deskContent hidden md:block">
+			<!-- svelte-ignore a11y_missing_attribute -->
 			<iframe
 				src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%231abc9c&ctz=America%2FLos_Angeles&showTitle=1&title=Plumas%20Disc%20Golf%20Events&showCalendars=0&showDate=1&src=Y19yb21mcjVtaWRzaGIxdm9uODVxZm92M29pOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23009688"
 				style="border: 0"
@@ -19,6 +25,7 @@
 			></iframe>
 		</div>
 		<div class="phoneContent md:hidden">
+			<!-- svelte-ignore a11y_missing_attribute -->
 			<iframe
 				src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%231abc9c&ctz=America%2FLos_Angeles&mode=AGENDA&showTabs=0&showCalendars=0&src=Y19yb21mcjVtaWRzaGIxdm9uODVxZm92M29pOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23009688"
 				style="border: 0"
