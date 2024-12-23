584f5f3f00654a9da4ea6f002cc50a0a5d3eb930
LabyrinthWarden <46149695+LabyrinthWarden@users.noreply.github.com>
2024-12-22 23:41:40 -0800

    more changes

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,10 @@
 import type { LayoutServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({ locals }) => {
+    if (locals.user) {
+        return {
+            user: locals.user
+        }
+    }
     return {};
 }) satisfies LayoutServerLoad;
