e30afacfa3c6cd6fc9a262fbfc7a72f0f4d6dc59
LabyrinthWarden <46149695+LabyrinthWarden@users.noreply.github.com>
2025-02-18 10:22:35 -0800

    made changes

---
+++ apps/web/src/routes/events/bag-tags/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	import { formatDate } from '$lib/utils';
+	import * as config from '$lib/config';
+	let { data } = $props();
+</script>
+
+<svelte:head>
+	<title>{config.title} - Bag Tags League</title>
+</svelte:head>
+
+<!-- Hero Section -->
+<div class="hero bg-base-200">
+	<div class="hero-content py-12 text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Bag Tags League</h1>
+		</div>
+	</div>
+</div>
+<div class="mt-8 grid w-full md:grid-cols-2">
+	<div>
+		<h2 class="mb-6 text-center text-3xl font-bold">What is a Bag Tags League?</h2>
+		<div class="prose lg:prose-xl mx-auto">
+			<p>Bag Tag leagues are a fun and competitive way to play disc golf! Here's the basic idea:</p>
+			<p>
+				<strong>Bag Tags:</strong> Each player in the league receives a numbered tag. Think of it like your ranking within the league. The lower the number on your tag, the higher your ranking.
+			</p>
+			<p>
+				<strong>Weekly Challenge:</strong> Each week, you'll play a round (or rounds) of disc golf, and your score determines your new tag number for the following week. The better you score, the lower your tag number becomes.
+			</p>
+			<p>
+				<strong>Challenge Matches:</strong> Outside of the regular weekly rounds, you can challenge other players for their tags. If you win the challenge match, you get their tag (and they get yours). This allows you to move up the rankings even faster.
+			</p>
+			<p>
+				<strong>Competition and Camaraderie:</strong> Bag tag leagues are a great way to compete against other players, improve your game, and enjoy the camaraderie of the disc golf community.
+			</p>
+			<p>For the Plumas Disc Golf Bag Tag Leagues Comprehensive Rules, click <a href="/events/bag-tags/rules">here</a></p>
+		</div>
+	</div>
+	<div>
+		<h2 class="my-6 text-center text-3xl font-bold">League Schedule</h2>
+		<div class="prose lg:prose-xl mx-auto">
+			<p><strong>Season Start:</strong> Plumas Disc Golf Membership Drive, April 5th, 2025. For more info, click <a href="">here</a>.</p>
+			<p>
+				<strong>Regular Season:</strong> Following the Membership Drive, leagues will follow the regular schedule.
+			</p>
+			<ul>
+				<li><strong>Ladies League:</strong> Mondays at 5:30 PM</li>
+				<li><strong>Open League:</strong> Wednesdays at 5:30 PM</li>
+			</ul>
+
+			<p>
+				<strong>Season End:</strong>
+			</p>
+			<ul>
+				<li><strong>Ladies League:</strong> September 15th, 2025</li>
+				<li><strong>Open League:</strong> September 17th, 2025</li>
+			</ul>
+			<p>
+				<strong>Location:</strong> Pioneer Disc Golf Course.
+			</p>
+			<p>
+				<strong>Course Closures (No League Play):</strong>
+			</p>
+			<ul>
+				<li>June 29th - July 11th (High Sierra Music Festival - Dates Subject to Change)</li>
+				<li>July 24th - July 28th (Plumas-Sierra County Fair - Dates Subject to Change)</li>
+				<li>Any other dates the course is closed to the public.</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
--- apps/web/src/routes/header.svelte
+++ apps/web/src/routes/header.svelte
@@ -7,28 +7,15 @@
 		<!-- Menu for small screens -->
 		<div class="dropdown">
 			<div tabindex="0" role="button" class="btn btn-ghost font-semibold text-white lg:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/>
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
 				</svg>
 			</div>
 			<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
-			<ul
-				tabindex="0"
-				class="menu dropdown-content menu-sm rounded-box bg-neutral z-[1] mt-3 w-52 p-2 font-semibold text-white shadow"
-			>
+			<ul tabindex="0" class="menu dropdown-content menu-sm rounded-box bg-neutral z-[1] mt-3 w-52 p-2 font-semibold text-white shadow">
 				<li><a href="/membership" aria-label="membership link">Membership</a></li>
 				<li><a href="/events" aria-label="events link">Events</a></li>
+				<li><a href="/events/bag-tags" aria-label="events link">Bag Tags League</a></li>
 				<li><a href="/courses" aria-label="courses link">Courses</a></li>
 				<li><a href="/calendar" aria-label="calendar link">Calendar</a></li>
 				<li><a href="/board" aria-label="board link">Board</a></li>
@@ -38,28 +25,26 @@
 			</ul>
 		</div>
 		<!-- Logo -->
-		<a href="/" class="btn btn-ghost hidden lg:block" aria-label="logo"
-			><img
-				src={logoImg}
-				alt="Plumas Disc Golf's Logo"
-				class="max-h-full scale-125 hover:scale-105"
-			/></a
-		>
+		<a href="/" class="btn btn-ghost hidden lg:block" aria-label="logo"><img src={logoImg} alt="Plumas Disc Golf's Logo" class="max-h-full scale-125 hover:scale-105" /></a>
 	</div>
 	<div class="navbar-center">
 		<div>
-			<a href="/" class="btn btn-ghost lg:hidden" aria-label="logo"
-				><img src={logoImg} alt="Plumas Disc Golf's Logo" class="max-h-full hover:scale-105" /></a
-			>
+			<a href="/" class="btn btn-ghost lg:hidden" aria-label="logo"><img src={logoImg} alt="Plumas Disc Golf's Logo" class="max-h-full hover:scale-105" /></a>
 		</div>
 		<!-- Menu for Larger screens -->
-		<ul class="menu menu-horizontal hidden px-1 font-semibold text-white lg:flex clear">
+		<ul class="menu menu-horizontal clear hidden px-1 font-semibold text-white lg:flex">
 			<li><a href="/membership" aria-label="membership link">Membership</a></li>
-			<li><a href="/events" aria-label="events link">Events</a></li>
+			<li>
+				<a href="/events" aria-label="events link">Events</a>
+				<ul class="clear bg-neutral mt-4 rounded-xl py-2">
+					<li><a href="/events">Event Listing</a></li>
+					<li><a href="/events/bag-tags">Bag Tags Leagues</a></li>
+				</ul>
+			</li>
 			<li>
 				<a href="/courses" aria-label="events link">Courses</a>
-				<ul class="clear bg-neutral rounded-xl mt-4 py-2">
-					<li ><a href="/courses/390f1cfs6hb80b0">Pioneer DGC</a></li>
+				<ul class="clear bg-neutral mt-4 rounded-xl py-2">
+					<li><a href="/courses/390f1cfs6hb80b0">Pioneer DGC</a></li>
 					<li><a href="/courses/0hwk22l0al0sr9m">Cascades DGC</a></li>
 				</ul>
 			</li>
@@ -76,38 +61,36 @@
 </div>
 
 <style>
+	/** ALL UL */
+	.navbar-center ul {
+		list-style: none;
 
-/** ALL UL */
-.navbar-center ul{
-    list-style: none;
-
-	z-index: 10;
-}
-/** ALL LI */
-.navbar-center ul li{
-
-    position: relative;
-}
-/** FIRST LEVEL LI */
-.navbar-center>ul>li{
-    float: left;
-}
-.navbar-center li ul{
-    display: none;
-    position: absolute;
-    top:20px;
-    left: 0;
-    width: 200px;
-}
-.navbar-center li:hover ul{
-    display: block;
-}
-.clear{
-    _zoom:1;
-}
-.clear:after{
-    content: "";
-    clear: both;
-    display: block;
-}
+		z-index: 10;
+	}
+	/** ALL LI */
+	.navbar-center ul li {
+		position: relative;
+	}
+	/** FIRST LEVEL LI */
+	.navbar-center > ul > li {
+		float: left;
+	}
+	.navbar-center li ul {
+		display: none;
+		position: absolute;
+		top: 20px;
+		left: 0;
+		width: 200px;
+	}
+	.navbar-center li:hover ul {
+		display: block;
+	}
+	.clear {
+		_zoom: 1;
+	}
+	.clear:after {
+		content: '';
+		clear: both;
+		display: block;
+	}
 </style>
