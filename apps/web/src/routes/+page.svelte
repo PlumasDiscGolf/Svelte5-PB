a1e26d49327cabf127f97c4ab0204f51c62b6102
LabyrinthWarden <labyrinthwarden13@gmail.com>
2025-02-21 16:32:04 -0800

    updated page

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -14,14 +14,9 @@
 	<div class="hero-content text-neutral-content h-full text-center">
 		<div class="mt-auto">
 			<div class="mb-4 grid h-full w-full items-end gap-2">
-				<h1 class="mb-5 text-5xl font-bold drop-shadow-[0_4px_3px_rgba(0,0,0,0.8)]">
-					Plumas Disc Golf
-				</h1>
-				<div class="min-h-content max-w-md rounded-xl bg-neutral bg-opacity-90 p-2">
-					<p class="my-auto">
-						Join the Plumas Disc Golf Family! Make new friends, play more disc golf, and support our
-						local courses.
-					</p>
+				<h1 class="mb-5 text-5xl font-bold drop-shadow-[0_4px_3px_rgba(0,0,0,0.8)]">Plumas Disc Golf</h1>
+				<div class="min-h-content bg-neutral max-w-md rounded-xl bg-opacity-90 p-2">
+					<p class="my-auto">Join the Plumas Disc Golf Family! Make new friends, play more disc golf, and support our local courses.</p>
 				</div>
 				<a href="/membership" class="btn btn-primary mx-auto">Join the Club</a>
 			</div>
@@ -58,66 +53,65 @@
 	</div>
 </div>
 
-<div id="menu">
-            <ul class="clear">
-                <li>
-                    <a href="">text 1</a>
-                    <ul class="clear">
-                        <li>text 1.1</li>
-                        <li>text 1.2</li>
-                        <li>text 1.3</li>
-                        <li>text 1.4</li>
-                    </ul>
-                </li>
-                <li>text 2</li>
-                <li>text 3</li>
-                <li>text 4</li>
-                <li>text 5</li>
-            </ul>
-        </div>
+<!-- <div id="menu">
+	<ul class="clear">
+		<li>
+			<a href="">text 1</a>
+			<ul class="clear">
+				<li>text 1.1</li>
+				<li>text 1.2</li>
+				<li>text 1.3</li>
+				<li>text 1.4</li>
+			</ul>
+		</li>
+		<li>text 2</li>
+		<li>text 3</li>
+		<li>text 4</li>
+		<li>text 5</li>
+	</ul>
+</div> -->
 
 <style>
-
-/** ALL UL */
-#menu ul{
-    list-style: none;
-    margin: 10px 0 0 0;
-    padding: 0;
-    background: #e0e0e0;
-    color: #747474;
-}
-/** ALL LI */
-#menu ul li{
-    border-right: 1px solid #ffffff;
-    padding: 5px 10px;
-    position: relative;
-}
-/** FIRST LEVEL LI */
-#menu>ul>li{
-    float: left;
-}
-#menu li ul{
-    display: none;
-    position: absolute;
-    top:20px;
-    left: 0;
-    width: 200px;
-}
-#menu li:hover ul{
-    display: block;
-}
-#menu li li{
-    border-bottom: 1px solid #ffffff;
-}
-#menu li li:hover{
-    background: #f0f0f0;
-}
-.clear{
-    _zoom:1;
-}
-.clear:after{
-    content: "";
-    clear: both;
-    display: block;
-}
+	/** ALL UL */
+	#menu ul {
+		list-style: none;
+		margin: 10px 0 0 0;
+		padding: 0;
+		background: #e0e0e0;
+		color: #747474;
+	}
+	/** ALL LI */
+	#menu ul li {
+		border-right: 1px solid #ffffff;
+		padding: 5px 10px;
+		position: relative;
+	}
+	/** FIRST LEVEL LI */
+	#menu > ul > li {
+		float: left;
+	}
+	#menu li ul {
+		display: none;
+		position: absolute;
+		top: 20px;
+		left: 0;
+		width: 200px;
+	}
+	#menu li:hover ul {
+		display: block;
+	}
+	#menu li li {
+		border-bottom: 1px solid #ffffff;
+	}
+	#menu li li:hover {
+		background: #f0f0f0;
+	}
+	.clear {
+		_zoom: 1;
+	}
+	.clear:after {
+		content: '';
+		clear: both;
+		display: block;
+	}
 </style>
