9916de95712bc2d6fd85366769ecfee089729c9f
LabyrinthWarden <labyrinthwarden13@gmail.com>
2025-02-24 14:50:10 -0800

    changed Zeffy form

--- src/routes/membership/+page.svelte
+++ src/routes/membership/+page.svelte
@@ -4,9 +4,7 @@
 
 <!-- Zeffy Script -->
 <svelte:head>
-	<script
-		src="https://zeffy-scripts.s3.ca-central-1.amazonaws.com/embed-form-script.min.js"
-	></script>
+	<script src="https://zeffy-scripts.s3.ca-central-1.amazonaws.com/embed-form-script.min.js"></script>
 </svelte:head>
 
 <!-- Hero Section -->
@@ -19,19 +17,12 @@
 </div>
 
 <div class="my-4 w-full text-center">
-	<p class="text-secondary mx-auto my-4 max-w-[72ch] font-semibold">
-		We are now processing online memberships through Zeffy, our non-profit financial partner. They
-		do not take any fees from our transactions, allowing us to use your full payment to help achieve
-		our goals.
-	</p>
+	<p class="mx-auto my-4 max-w-[72ch] font-semibold text-secondary">We are now processing online memberships through Zeffy, our non-profit financial partner. They do not take any fees from our transactions, allowing us to use your full payment to help achieve our goals.</p>
 </div>
 
 <div class="grid">
 	<div class="m-auto max-w-[72ch] px-4">
-		<p class="my-4 font-semibold">
-			Becoming a Club Member is one of the best ways to support Plumas Disc Golf. Memberships are
-			one of our primary fundraising sources each year. Thank you for your support!
-		</p>
+		<p class="my-4 font-semibold">Becoming a Club Member is one of the best ways to support Plumas Disc Golf. Memberships are one of our primary fundraising sources each year. Thank you for your support!</p>
 		<p class="font-semibold">The Standard Membership includes:</p>
 		<ul class="ml-3">
 			<li>A Custom PDG Bag Tag</li>
@@ -39,19 +30,13 @@
 			<li>Discounted entry into club events for the year</li>
 		</ul>
 	</div>
-	<div class="flex w-full justify-center mt-4">
+	<!-- <div class="flex w-full justify-center mt-4">
 		<button
 			zeffy-form-link="https://www.zeffy.com/embed/ticketing/club-membership--2025?modal=true"
 			class="btn btn-secondary p">Join Plumas Disc Golf</button
 		>
-	</div>
-	<!-- <div style="position:relative;overflow:hidden;height:500px;width:100%;padding-top:500px;">
-		<iframe
-			title="Donation form powered by Zeffy"
-			style="position: absolute; border: 0; top:0;left:0;bottom:0;right:0;width:100%;height:100%"
-			src="https://www.zeffy.com/embed/ticketing/7db2218f-945d-49ad-9125-628c75b8b13c"
-			allowpaymentrequest
-			allowTransparency="true"
-		></iframe>
 	</div> -->
+	<div style="position:relative;overflow:hidden;height:500px;width:100%;padding-top:500px;">
+		<iframe title="Membership form powered by Zeffy" style="position: absolute; border: 0; top:0;left:0;bottom:0;right:0;width:100%;height:100%" src="https://www.zeffy.com/embed/ticketing/club-membership--2025" allowpaymentrequest allowTransparency="true"></iframe>
+	</div>
 </div>
