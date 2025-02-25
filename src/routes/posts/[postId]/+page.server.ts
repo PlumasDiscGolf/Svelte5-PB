b79a9269c661daaa4e199d4b5122ef1f0ddf3e69
Jeremiah Bridges <labyrinthwarden13@gmail.com>
2025-02-25 14:29:10 -0800

    fixing posts

--- src/routes/posts/[postId]/+page.server.ts
+++ src/routes/posts/[postId]/+page.server.ts
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals, params }) {
-	const course = await locals.pb.collection('courses').getOne(params.courseId);
+	const course = await locals.pb.collection('courses').getOne(params.postId);
 	return { course };
 }
  export const prerender = false
