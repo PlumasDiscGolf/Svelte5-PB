e52c9f4444326b9a839214f374c5da47bf504db8
LabyrinthWarden <46149695+LabyrinthWarden@users.noreply.github.com>
2025-02-25 21:30:03 -0800

    fixed post details page

--- src/routes/posts/[postId]/+page.server.ts
+++ src/routes/posts/[postId]/+page.server.ts
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals, params }) {
-	const course = await locals.pb.collection('courses').getOne(params.postId);
-	return { course };
+	const post = await locals.pb.collection('posts').getOne(params.postId);
+	return { post };
 }
  export const prerender = false
--- src/routes/posts/[postId]/+page.svelte
+++ src/routes/posts/[postId]/+page.svelte
@@ -9,7 +9,7 @@
 <div class="hero bg-base-200">
 	<div class="hero-content py-12 text-center">
 		<div class="max-w-lg">
-			<h1 class="text-5xl font-bold">Pioneer DGC Tee Pad Project is Complete</h1>
+			<h1 class="text-5xl font-bold">{data.post.title}</h1>
 		</div>
 	</div>
 </div>
@@ -20,36 +20,7 @@
 		<div class="mx-auto max-w-4xl">
 			<!-- Article Content -->
 			<div class="prose prose-lg max-w-none">
-				<p>
-					The Pioneer Tee Pad Project is complete and it's AMAZING! This wouldn't have been possible
-					without the incredible support of our community. A heartfelt thank you to:
-				</p>
-				<ul>
-					<li>
-						<strong>Our Donors:</strong> Bread for the Journey Feather River and Feather River Food Co-Op's
-						Round Up at the Register program for funding the tee pad materials and signage. Your generosity
-						made this project a reality! Nick Maffei for providing funds for volunteer supplies and food,
-						and Mike McQuinn for the charcoal!
-					</li>
-					<li>
-						<strong>Our Volunteers:</strong> Chase Ramirez for his vision and hard work in organizing
-						this project! Klay Lund for bringing his tractor and saving us tons of time and effort! And
-						a HUGE thank you to Rebecca Terhune, Mel Strahm, Justin Cooke, Dennis Williams, Mark Jennings,
-						Rick Stock, Frederick Howe, Matthew Brooks, Eric Jobe, Ben Williamson, Dan Ramirez, Alex
-						Lemnah, and Blake Bridges for putting in the sweat equity to get those tee pads installed!
-					</li>
-					<li>
-						<strong>Our Amazing Cooks:</strong> Frederick Howe for bringing the grill, Joe &amp; Wendy
-						Bridges for the delicious potato salad, and Jeremiah Bridges for grilling up a feast for
-						everyone!
-					</li>
-				</ul>
-				<p>
-					Because of you, Pioneer DGC has an awesome newly upgraded disc golf course that everyone
-					can enjoy! But wait, there's more! We're not done yet! Stay tuned for updates as we
-					finalize the basket locations, create a course map, and install those awesome new tee
-					signs! Exciting things are happening at Pioneer DGC!
-				</p>
+				{@html data.post.content}
 			</div>
 
 			<!-- Tags -->
