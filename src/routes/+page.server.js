93e57e94670f2134e137e2596e2b2f2315453fba
Jeremiah Bridges <labyrinthwarden13@gmail.com>
2025-08-29 16:25:05 -0700

    added json stringify

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,8 @@
 export async function load({ locals }) {
 	const posts = await locals.pb.collection('posts').getList(1, 3, {
 		sort: '-publishedDate',
-        filter: 'published = true'
+		filter: 'published = true'
 	});
 
-	return { posts };
+	return { posts: JSON.parse(JSON.stringify(posts)) };
 }
